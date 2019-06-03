b433b625eeac2264c85288996fac085809eb06da
jyebe <jyebe9034@gmail.com>
2019-06-03 13:53:06 +0900

    updated all by jihye. 0603-1:53

---
+++ src/kh/semi/dao/MemberDAO.java
@@ -0,0 +1,318 @@
+package kh.semi.dao;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import kh.semi.dto.MemberDTO;
+
+public class MemberDAO {
+	public Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "semi";
+		String pw = "semi";
+		return DriverManager.getConnection(url, user, pw);
+	}
+
+	public static String testSHA256(String str){
+		String SHA = ""; 
+		try{
+			MessageDigest sh = MessageDigest.getInstance("SHA-256"); 
+			sh.update(str.getBytes()); 
+			byte byteData[] = sh.digest();
+			StringBuffer sb = new StringBuffer(); 
+			for(int i = 0 ; i < byteData.length ; i++){
+				sb.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
+			}
+			SHA = sb.toString();
+		}catch(NoSuchAlgorithmException e){
+			e.printStackTrace(); 
+			SHA = null; 
+		}
+		return SHA;
+	}
+
+
+	public int insertNaverMember(MemberDTO param) throws Exception{
+
+		String sql = "insert into members (m_email,m_name,m_joindate,m_ipaddress,m_admin) values(?,?,default,?,'n')";
+
+		try(Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);){
+
+			pstat.setString(1, param.getEmail());
+			pstat.setString(2, param.getName());
+			pstat.setString(3, param.getIpAddress());
+
+			int result = pstat.executeUpdate();
+
+			con.commit();
+			return result;
+		}
+	}
+
+	public boolean isIdExist(MemberDTO param) throws Exception{
+
+		String sql = "select * from members where m_email = ?";
+
+		try(Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);){
+
+			pstat.setString(1, param.getEmail());
+
+			try(ResultSet rs = pstat.executeQuery();){
+
+				return rs.next();
+			}
+		}
+
+	}
+
+	public MemberDTO NaverContentsParse(String res, String ipaddr) throws Exception{
+
+		JsonParser parser = new JsonParser();
+		JsonObject ob = (JsonObject) parser.parse(res.toString());
+		String access_t = ob.get("access_token").getAsString();
+
+		String token = access_t;// 네이버 로그인 접근 토큰;
+		String header = "Bearer " + token; // Bearer 다음에 공백 추가
+
+		String apiURL1 = "https://openapi.naver.com/v1/nid/me";
+		URL url1 = new URL(apiURL1);
+		HttpURLConnection con1 = (HttpURLConnection)url1.openConnection();
+		con1.setRequestMethod("GET");
+		con1.setRequestProperty("Authorization", header);
+		int responseCode1 = con1.getResponseCode();
+		BufferedReader br1;
+		if(responseCode1==200) { // 정상 호출
+			br1 = new BufferedReader(new InputStreamReader(con1.getInputStream()));
+		} else {  // 에러 발생
+			br1 = new BufferedReader(new InputStreamReader(con1.getErrorStream()));
+		}
+		String inputLine1;
+		StringBuffer response1 = new StringBuffer();
+		while ((inputLine1 = br1.readLine()) != null) {
+			response1.append(inputLine1);
+		}
+		br1.close();
+
+		String ob2 = response1.toString();
+		JsonObject Jsonob2 = parser.parse(ob2).getAsJsonObject();
+		JsonObject Json_response = Jsonob2.get("response").getAsJsonObject();
+
+		String id = Json_response.get("id").getAsString();
+		String name = Json_response.get("name").getAsString();
+		String email = Json_response.get("email").getAsString();
+
+		System.out.println(id +  name + email);
+
+		MemberDTO dto = new MemberDTO();
+		dto.setEmail(email);
+		dto.setIpAddress(ipaddr);
+		dto.setName(name);
+
+		return dto;
+	}
+
+
+
+	public int insertMember(MemberDTO dto) throws Exception {
+		String sql = "insert into members values (?,?,?,?,?,?,?,default,?,?)";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				){
+			pstat.setString(1, dto.getEmail());
+			pstat.setString(2, testSHA256(dto.getPw()));
+			pstat.setString(3, dto.getName());
+			pstat.setString(4, dto.getPhone());
+			pstat.setString(5, dto.getZipCode());
+			pstat.setString(6, dto.getAddress1());
+			pstat.setString(7, dto.getAddress2());
+			pstat.setString(8, dto.getIpAddress());
+			pstat.setString(9, dto.getAdmin());	
+			int result = pstat.executeUpdate();
+			con.commit();
+			return result;
+		}
+	}
+
+	public List<MemberDTO> selectAll() throws Exception {
+		String sql = "select * from members";
+		List<MemberDTO> result = new ArrayList<>();
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();
+				){
+			while(rs.next()) {
+				MemberDTO dto = new MemberDTO();
+				dto.setEmail(rs.getString(1));
+				dto.setPw(rs.getString(2));
+				dto.setName(rs.getString(3));
+				dto.setPhone(rs.getString(4));
+				dto.setZipCode(rs.getString(5));
+				dto.setAddress1(rs.getString(6));
+				dto.setAddress2(rs.getString(7));
+				dto.setJoinDate(rs.getTimestamp(8));
+				dto.setIpAddress(rs.getString(9));
+				dto.setAdmin(rs.getString(10));
+				result.add(dto);
+			}
+			return result;
+		}
+	}
+	
+	public List<String> selectByEmail(String email) throws Exception {
+		String sql = "select m_name, m_email, m_phone from members where m_email='"+email+"'";
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = con.prepareStatement(sql);
+				ResultSet rs = pstat.executeQuery();
+				){
+			if(rs.next()) {
+				List<String> result = new ArrayList<>();
+				result.add(rs.getString(1));
+				result.add(rs.getString(2));
+				result.add(rs.getString(3));
+				return result;
+			}
+			return null;
+		}
+	}
+
+	public PreparedStatement pstatForIsLoginOk(Connection con, String email, String pw) throws Exception {
+		String sql = "select * from members where m_email=? and m_pw=?";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		pstat.setString(1, email);
+		pstat.setString(2, testSHA256(pw));
+
+		return pstat;
+	}
+
+	public boolean isLoginOk(String email, String pw) throws Exception {
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = this.pstatForIsLoginOk(con, email, pw);
+				ResultSet rs = pstat.executeQuery();
+				){
+			return rs.next();
+		}
+	}
+
+	public PreparedStatement pstatForEmailDuplCheck(Connection con, String email) throws Exception {
+		String sql = "select * from members where m_email=?";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		pstat.setString(1, email);
+		return pstat;
+	}
+	public boolean emailDuplCheck(String email) throws Exception {
+		try(
+				Connection con = this.getConnection();
+				PreparedStatement pstat = this.pstatForEmailDuplCheck(con, email);
+				ResultSet rs = pstat.executeQuery();
+				){
+			if(rs.next()) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
+	public static int sendMail(String email) {
+		Properties p = System.getProperties();
+		p.put("mail.smtp.starttls.enable", "true");     // gmail은 무조건 true 고정
+		p.put("mail.smtp.host", "smtp.gmail.com");      // smtp 서버 주소
+		p.put("mail.smtp.auth","true");                 // gmail은 무조건 true 고정
+		p.put("mail.smtp.port", "587");                 // gmail 포트
+
+		Authenticator auth = new MyAuthentication();
+
+		//session 생성 및  MimeMessage생성
+		Session session = Session.getDefaultInstance(p, auth);
+		MimeMessage msg = new MimeMessage(session);
+		int ranNum = getRandomNum();
+		try{
+			//편지보낸시간
+			msg.setSentDate(new Date());
+
+			InternetAddress from = new InternetAddress() ;
+
+			// 이메일 발신자
+			from = new InternetAddress("RUN UP <//email>");
+			msg.setFrom(from);
+
+			// 이메일 수신자
+			InternetAddress to = new InternetAddress(email);
+			msg.setRecipient(Message.RecipientType.TO, to);
+
+			// 이메일 제목
+			msg.setSubject("*도움닿기* 회원가입 인증 메일", "UTF-8");
+
+			// 이메일 내용
+			msg.setText("<h3>\"도움닿기\" 회원가입 인증 메일입니다.</h3><br>"
+					+ "아래 인증번호를 입력창에 입력해주세요.<br>"
+					+ "인증번호는 " + ranNum + "입니다.", "UTF-8");
+
+			// 이메일 헤더
+
+			msg.setHeader("content-Type", "text/html");
+
+			//메일보내기
+			javax.mail.Transport.send(msg);
+
+		}catch (AddressException addr_e) {
+			addr_e.printStackTrace();
+			return 0;
+		}catch (MessagingException msg_e) {
+			msg_e.printStackTrace();
+			return 0;
+		}
+		return ranNum;
+	}
+
+	public static int getRandomNum() {
+
+		int ranNum = (int)(Math.random()*(999999-100000+1)+100000);
+
+		return ranNum;
+	}
+}
+
+class MyAuthentication extends Authenticator {
+
+	PasswordAuthentication pa;
+	public MyAuthentication(){
+		String id = "@gmail.com";       // 구글 ID
+		String pw = "";          // 구글 비밀번호
+		// ID와 비밀번호를 입력한다.
+		pa = new PasswordAuthentication(id, pw);
+	}
+	// 시스템에서 사용하는 인증정보
+	public PasswordAuthentication getPasswordAuthentication() {
+		return pa;
+	}
+}
---
+++ src/kh/semi/servlet/BoardController.java
@@ -0,0 +1,415 @@
+package kh.semi.servlet;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import kh.semi.dao.BoardDAO;
+import kh.semi.dao.MemberDAO;
+import kh.semi.dao.PaymentDAO;
+import kh.semi.dao.TitleImgDAO;
+import kh.semi.dto.BoardDTO;
+import kh.semi.dto.CommentDTO;
+import kh.semi.dto.PaymentDTO;
+import kh.semi.dto.TitleImgDTO;
+
+
+@WebServlet("*.board")
+public class BoardController extends HttpServlet {	
+	private int fileIdNo = 1;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");// 페이지내에 인코딩 정보가 없는 경우에 필요한 코드(ex. ajax)
+		PrintWriter pw = response.getWriter();
+		String requestURI = request.getRequestURI(); 
+		String contextPath = request.getContextPath();
+
+		String cmd = requestURI.substring(contextPath.length());
+
+		MemberDAO mdao = new MemberDAO();
+		BoardDAO dao = new BoardDAO();
+		PaymentDAO pdao = new PaymentDAO();
+		TitleImgDAO tdao = new TitleImgDAO();
+		BoardDTO dto = new BoardDTO();
+		TitleImgDTO tdto = new TitleImgDTO();
+
+		try {
+			if(cmd.contentEquals("/titleImagesMain.board")) {
+				List<BoardDTO> list1 = dao.getDataForMain();
+				int bNo1 = list1.get(0).getBoardNo();
+				int bNo2 = list1.get(1).getBoardNo();
+				int bNo3 = list1.get(2).getBoardNo();
+				
+				List<TitleImgDTO> list2 = tdao.getTitleImgMain(bNo1, bNo2, bNo3);
+				String fName1 = list2.get(0).getFileName();
+				String fPath1 = list2.get(0).getFilePath();
+				String imgTag1 = fPath1 + fName1;
+				
+				String fName2 = list2.get(1).getFileName();
+				String fPath2 = list2.get(1).getFilePath();
+				String imgTag2 = fPath2 + fName2;
+				
+				String fName3 = list2.get(2).getFileName();
+				String fPath3 = list2.get(2).getFilePath();
+				String imgTag3 = fPath3 + fName3;
+				
+			}else if(cmd.contentEquals("/card1.board")) {
+				List<BoardDTO> list = dao.getDataForMain();
+							
+				String title1 = list.get(0).getTitle();
+				int goalAmount1 = list.get(0).getAmount();
+				
+				Timestamp dueDate1 = list.get(0).getDueDate();
+				long dueTime1 = dueDate1.getTime();
+				SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+				String dueDateStr1 = sdf1.format(dueTime1);
+				
+				int sumAmount1 = list.get(0).getSumAmount();
+				double percentage1 = Math.floor((double)sumAmount1 / goalAmount1 * 100);
+				// 마감 임박 1
+
+				JsonObject obj1 = new JsonObject();
+				obj1.addProperty("title1", title1);
+				obj1.addProperty("dueDate1",dueDateStr1);
+				obj1.addProperty("percentage1",percentage1);
+				pw.print(obj1.toString());
+			}else if(cmd.contentEquals("/card2.board")) {
+				List<BoardDTO> list = dao.getDataForMain();
+				
+				String title2 = list.get(1).getTitle();
+				int goalAmount2 = list.get(1).getAmount();
+				Timestamp dueDate2 = list.get(1).getDueDate();
+				long dueTime2 = dueDate2.getTime();
+				SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+				String dueDateStr2 = sdf2.format(dueTime2);
+				int sumAmount2 = list.get(1).getSumAmount();
+				double percentage2 = Math.floor((double)sumAmount2 / goalAmount2 * 100);
+				 // 마감 임박2
+				
+				JsonObject obj2 = new JsonObject();
+				obj2.addProperty("title2", title2);
+				obj2.addProperty("dueDate2",dueDateStr2);
+				obj2.addProperty("percentage2",percentage2);
+				pw.print(obj2.toString());
+			}else if(cmd.contentEquals("/card3.board")) {
+				List<BoardDTO> list = dao.getDataForMain();
+				
+				String title3 = list.get(2).getTitle();
+				int goalAmount3 = list.get(2).getAmount();
+				Timestamp dueDate3 = list.get(2).getDueDate();
+				long dueTime3 = dueDate3.getTime();
+				SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd");
+				String dueDateStr3 = sdf3.format(dueTime3);
+				int sumAmount3 = list.get(2).getSumAmount();
+				double percentage3 = Math.floor((double)sumAmount3 / goalAmount3 * 100);
+				 // 마감 임박 3
+				
+				JsonObject obj3 = new JsonObject();
+				obj3.addProperty("title3", title3);
+				obj3.addProperty("dueDate3",dueDateStr3);
+				obj3.addProperty("percentage3",percentage3);
+				pw.print(obj3.toString());
+			}else if(cmd.contentEquals("/totalAmountDonors.board")) {
+				
+				int totalAmount = pdao.getTotalAmount();
+				int countDonors = pdao.getNumberOfDonors();
+				
+				JsonObject obj = new JsonObject();
+				obj.addProperty("totalAmount", totalAmount);
+				obj.addProperty("countDonors", countDonors);
+				pw.print(obj.toString());
+
+			}else if(cmd.contentEquals("/write.board")) {
+				request.getRequestDispatcher("/WEB-INF/boards/writer.jsp").forward(request, response);
+				
+			}else if(cmd.equals("/writer.board")) { // 사용자가 입력한 값을 받아서 BoardDAO로 보내주는 부분
+				request.getSession().setAttribute("flag", "true");
+				int maxSize = 10 * 1024 * 1024;
+
+				SimpleDateFormat simpledf = new SimpleDateFormat("yy-MM-dd");
+				Long tempTime = System.currentTimeMillis();
+				String newDate = simpledf.format(tempTime);
+
+				String rootPath = request.getSession().getServletContext().getRealPath("/");
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				dto.setEmail(email);
+
+				File tempFile = new File(rootPath+email);
+				if(!tempFile.exists()) {
+					tempFile.mkdir();
+				}
+
+				String savePath = rootPath + email + "/" + newDate;
+				tdto.setFilePath(savePath);
+				String uploadFile = "";
+				String newFileName = "";
+
+				File uploadPath = new File(savePath);
+				if(!uploadPath.exists()) {
+					uploadPath.mkdir();
+				}
+
+				long currentTime = System.currentTimeMillis();
+				SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+
+				try {
+					MultipartRequest multi = new MultipartRequest(request, savePath, maxSize, "UTF-8", new DefaultFileRenamePolicy());
+					
+					uploadFile = multi.getFilesystemName("filename");
+					newFileName = currentTime + "." + uploadFile.substring(uploadFile.lastIndexOf(".")+1);
+					tdto.setFileName(newFileName);
+
+					File oldFile = new File(savePath + "/" + uploadFile);
+					File newFile = new File(savePath  + "/" + newFileName);
+					
+					if(!oldFile.renameTo(newFile)) {
+						DataOutputStream dos = new DataOutputStream(new FileOutputStream(newFile));
+						DataInputStream dis = new DataInputStream(new FileInputStream(oldFile));
+
+						byte[] fileContents = new byte[(int)oldFile.length()];
+						dis.readFully(fileContents);
+
+						dos.write(fileContents);
+						dos.flush();
+						dos.close();
+						dis.close();
+						oldFile.delete();
+					}
+					
+					dto.setTitle(multi.getParameter("title"));
+					dto.setWriter(multi.getParameter("writer"));
+					dto.setAmount(Integer.parseInt(multi.getParameter("amount")));
+					String duedate = multi.getParameter("dueDate") + " " + sdf.format(currentTime);
+					dto.setDueDate(Timestamp.valueOf(duedate));
+					dto.setBank(multi.getParameter("select"));
+					dto.setAccount(multi.getParameter("account"));
+					dto.setContents(multi.getParameter("contents"));
+					try {
+						int result = dao.insertBoard(dto);
+						System.out.println("result : "+result);
+					}catch(Exception e) {
+						e.printStackTrace();
+					}
+					int result = dao.insertTitleImg(tdto);
+					System.out.println("titleresult : " + result);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}else if(cmd.equals("/uploadImage.board")) { // 서버 측 이미지 업로드 
+				request.getSession().setAttribute("flag", "false");
+				int maxSize = 10 * 1024 * 1024;
+
+				SimpleDateFormat simpledf = new SimpleDateFormat("yy-MM-dd");
+				Long tempTime = System.currentTimeMillis();
+				String newDate = simpledf.format(tempTime);
+
+				String rootPath = request.getSession().getServletContext().getRealPath("/");
+				String email = (String)request.getSession().getAttribute("loginEmail");
+
+				File tempFile = new File(rootPath+email);
+				if(!tempFile.exists()) {
+					tempFile.mkdir();
+				}
+
+				String savePath = rootPath + email + "/" + newDate;
+				tdto.setFilePath(savePath);
+				String uploadFile = "";
+				String newFileName = "";
+
+				File uploadPath = new File(savePath);
+				if(!uploadPath.exists()) {
+					uploadPath.mkdir();
+				}
+
+				long currentTime = System.currentTimeMillis();
+
+				try {
+					MultipartRequest multi = new MultipartRequest(request, savePath, maxSize, "UTF-8", new DefaultFileRenamePolicy());				
+					uploadFile = multi.getFilesystemName("file");
+					newFileName = currentTime + "." + uploadFile.substring(uploadFile.lastIndexOf(".")+1);
+					tdto.setFileName(newFileName);
+
+					File oldFile = new File(savePath + "/" + uploadFile);
+					File newFile = new File(savePath  + "/" + newFileName);
+					
+					if(!oldFile.renameTo(newFile)) {
+						DataOutputStream dos = new DataOutputStream(new FileOutputStream(newFile));
+						DataInputStream dis = new DataInputStream(new FileInputStream(oldFile));
+
+						byte[] fileContents = new byte[(int)oldFile.length()];
+						dis.readFully(fileContents);
+
+						dos.write(fileContents);
+						dos.flush();
+						dos.close();
+						dis.close();
+						oldFile.delete();
+					}
+
+					pw.append(email + "/" + newDate + "/"+ newFileName);
+
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+
+			}else if(cmd.equals("/deleteImage.board")) { // 서버 측 이미지 삭제
+				try {
+					Thread.sleep(500);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+
+				String test = (String)request.getSession().getAttribute("flag");
+				System.out.println("test : " + test);
+				if(test.equals("false")) {
+					String rootPath = this.getServletContext().getRealPath("/");
+					String fileUrl = request.getParameter("src");
+					System.out.println("fileUrl : " + fileUrl);
+					String filePath = null;
+					if(fileUrl.startsWith("http")) {
+						filePath = fileUrl.replaceAll("http://.+?/", "");
+					}else {
+						filePath = fileUrl;
+					}
+					boolean deleteFile = new File(rootPath+filePath).delete();
+					pw.print(deleteFile);
+				}
+				request.getSession().setAttribute("flag", "false");
+				
+			}else if(cmd.equals("/Read.board")) {
+				int commentPage = Integer.parseInt(request.getParameter("commentPage"));
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				BoardDTO article = dao.selectOneArticle(boardNo);
+				List<CommentDTO> comments = dao.selectCommentsByBoardNo(commentPage, boardNo);
+				
+				double amount = article.getAmount();
+				double sumAmount = article.getSumAmount();
+				double percentage = Math.floor((double)sumAmount / amount * 100);
+
+//				TitleImgDTO titleImg = dao.getTitleImg(boardNo);
+				//				response.setCharacterEncoding("UTF-8");
+				//				request.setCharacterEncoding("UTF-8");
+				//				request.setAttribute("titleImg", "files\\" + titleImg.getFileName());
+				//				System.out.println("files\\" + titleImg.getFileName());
+
+				DecimalFormat Commas = new DecimalFormat("#,###,###");
+
+				request.setAttribute("comments", comments);
+				request.setAttribute("percentage", percentage);
+				request.setAttribute("boardNo", boardNo);
+				request.setAttribute("amount", Commas.format(amount));
+				request.setAttribute("sumAmount", Commas.format(sumAmount));
+				request.setAttribute("result", article);
+
+				request.getRequestDispatcher("read.jsp").forward(request, response);
+
+			}else if(cmd.equals("/PaymentForm.board")){
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				String title = dao.selectOneArticle(boardNo).getTitle();
+
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				List<String> result = mdao.selectByEmail(email);
+				request.setAttribute("boardNo", boardNo);
+				request.setAttribute("title", title);
+				request.setAttribute("result", result);
+				request.getRequestDispatcher("payment.jsp").forward(request, response);
+				
+			}else if(cmd.equals("/Payment.board")) {
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				String name = request.getParameter("name");
+				String email = request.getParameter("email");
+				String phone = request.getParameter("phone");
+				int amount = Integer.parseInt(request.getParameter("amount"));
+
+				try {
+					PaymentDTO pdto = new PaymentDTO(boardNo, name, email, phone, amount, null);
+					int result = pdao.insertPayment(pdto);
+					int result2 = dao.updateSumAccount(amount, boardNo);
+
+					BoardDTO board = dao.selectOneArticle(boardNo);
+
+					request.setAttribute("boardNo", boardNo);
+					request.setAttribute("result", result);
+					request.setAttribute("board", board);
+					request.setAttribute("payment", dto);
+					request.getRequestDispatcher("/WEB-INF/boards/payCompleted.jsp").forward(request, response);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}else if(cmd.equals("/Recommend.board")) {
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				String title = request.getParameter("title");
+
+				try {
+					dao.updateRecommend(email, boardNo, title);
+					int recommend = dao.selectOneArticle(boardNo).getRecommend();
+					pw.print(recommend);
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}else if(cmd.equals("/RecommendCheck.board")) {
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				
+				try {
+					pw.print(dao.recommendCheck(email, boardNo));
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}else if(cmd.equals("/Comment.board")) {
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+				String comment = request.getParameter("comment");
+				int currentPage = Integer.parseInt(request.getParameter("currentPage"));
+				
+				try {
+					String name = mdao.selectByEmail(email).get(0);
+					
+					dao.insertComment(email, name, boardNo, comment);
+					CommentDTO result = dao.selectCommentsByBoardNo(currentPage, boardNo).get(0);
+					Gson gson = new Gson();
+					pw.print(gson.toJson(result));
+				}catch(Exception e) {
+					e.printStackTrace();
+				}
+			}else if(cmd.equals("/DeleteComment.board")) {
+				String email = (String)request.getSession().getAttribute("loginEmail");
+				String writeDate = request.getParameter("writeDate");
+				System.out.println(email + " : " + writeDate);
+				
+				int result = dao.deleteComment(email, writeDate);
+				System.out.println(result);
+				pw.print(result);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
